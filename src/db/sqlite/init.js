2d206ad31fe7358727ee57ba35689c2395bb3c7d
Dávid Szabó <david.szabo97@gmail.com>
2017-05-20 12:50:34 +0200

    Add syntax_id column to paste table

--- src/db/sqlite/init.js
+++ src/db/sqlite/init.js
@@ -4,6 +4,7 @@ module.exports = async function init(db) {
       '(`id` INTEGER PRIMARY KEY AUTOINCREMENT,' +
       '`title` TEXT NOT NULL DEFAULT \'cp\',' +
       '`visibility` INTEGER NOT NULL DEFAULT 0,' +
+      '`syntax_id` INTEGER NOT NULL DEFAULT 0,' +
       '`created_at` INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP )'),
 
     db.runAsync('CREATE TABLE IF NOT EXISTS `syntax`' +
