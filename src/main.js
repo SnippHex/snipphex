1274ef0f85d93486936f8c9b4aec7e50ee2a8fd8
Dávid Szabó <david.szabo97@gmail.com>
2017-05-20 12:02:13 +0200

    Add body-parser

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const logger = require('src/logger')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(bodyParser.json({ limit: '10mb' }))
 app.use(require('src/paste/router'))
 app.use(require('src/syntax/router'))
 app.use(require('src/theme/router'))
