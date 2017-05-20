6f3732b7b14f37ca5d08b88027d6237e950d0e2b
Dávid Szabó <david.szabo97@gmail.com>
2017-05-20 11:56:47 +0200

    Populate syntax database on db-init

--- scripts/init-db.js
+++ scripts/init-db.js
@@ -1,3 +1,11 @@
 require('../bootstrap')
 require('src/db')
-require('src/db/init')
+const initPromise = require('src/db/init')
+const syntaxService = require('src/syntax/service')
+const syntaxes = require('./data/syntaxes')
+
+if (initPromise) {
+  initPromise.then(() => {
+    syntaxes.forEach(v => syntaxService.create(v))
+  })
+}
