7fb72e3ffc42735f8a7c83d3f647f5bcaf7e7a97
Dávid Szabó <david.szabo97@gmail.com>
2017-05-20 10:42:54 +0200

    Add paste sqlite repository

---
+++ src/paste/repository/sqlite.js
@@ -0,0 +1,24 @@
+const promisify = require('promisify-node')
+const db = promisify(require('src/db'))
+const pasteKeyCoder = require('src/paste/key-coder')
+
+async function getById(id) {
+  return db.all('SELECT id, title, visiblity FROM paste WHERE id = ?', [id])
+}
+
+async function getByKey(key) {
+  const id = pasteKeyCoder.decode(key)
+
+  return getById(id)
+}
+
+async function create(data) {
+  return db.run('INSERT INTO paste SET title = ?, visiblity = ?, syntax_id = ?', [data.title, data.visibility, data.syntax_id])
+    .then(result => result.lastID)
+}
+
+module.exports = {
+  getById,
+  getByKey,
+  create,
+}
