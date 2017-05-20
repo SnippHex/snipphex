e4a7750ba6ae9ef0439bbc109a44201a98ff6382
Dávid Szabó <david.szabo97@gmail.com>
2017-05-20 09:46:11 +0200

    Add paste key encode and decode

---
+++ src/paste/key-coder.js
@@ -0,0 +1,26 @@
+const codeMap = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_'
+const base = codeMap.length
+
+module.exports = {
+  encode: function encode(id) {
+    --id
+    let key = ''
+
+    do {
+      key += codeMap.charAt(id % base)
+      id = Math.floor(id / base)
+    } while (id > 0)
+
+    return key.split('').reverse().join('')
+  },
+
+  decode: function decode(key) {
+    let id = 0
+
+    for (let i = 0, len = key.length; i < len; i++) {
+      id = id * base + codeMap.indexOf(key.charAt(i))
+    }
+
+    return ++id
+  },
+}
