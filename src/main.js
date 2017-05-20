0313bce001553088beb8fdaa96970b040e1fd39b
Dávid Szabó <david.szabo97@gmail.com>
2017-05-20 07:58:54 +0200

    Use routers

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ const logger = require('src/logger')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(require('src/paste/router'))
+app.use(require('src/syntax/router'))
+app.use(require('src/theme/router'))
+
 app.listen(port, () => {
   logger.info(`HTTP Server listening on port ${port}!`)
 })
