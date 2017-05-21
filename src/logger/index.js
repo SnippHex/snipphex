77703201569039b20f059951837ead75d33e1d50
Dávid Szabó <david.szabo97@gmail.com>
2017-05-21 08:13:55 +0200

    Add file logging and error filtering

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,5 +1,24 @@
 const winston = require('winston')
+const path = require('path')
 
-winston.level = process.env.LOG_LEVEL || 'debug'
+const logger = new winston.Logger({
+  level: process.env.LOG_LEVEL || 'debug',
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({ filename: path.join(ROOT, 'logs', 'log.log') }),
+  ],
+  filters: [
+    function filterErrors(level, msg, meta) {
+      if (meta && meta.err) {
+        meta.err = {
+          message: meta.err.message,
+          stack: meta.err.stack,
+        }
+      }
 
-module.exports = winston
+      return msg
+    },
+  ],
+})
+
+module.exports = logger
