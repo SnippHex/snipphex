8112070373cc6b0b8143d58d9eff81b5217c3de4
Dávid Szabó <david.szabo97@gmail.com>
2017-05-21 08:22:30 +0200

    Add caching of themes

--- src/theme/service.js
+++ src/theme/service.js
@@ -1,12 +1,20 @@
 const pygments = require('src/pygments')
 const os = require('os')
 
+let stylesCache
+
 async function all() {
+  if (stylesCache) {
+    return stylesCache
+  }
+
   const stylesOut = await pygments.exec(['-L', 'styles'])
   const lines = stylesOut.split(os.EOL)
   const styles = lines.slice(4).filter((v, k) => k % 2 === 0 && v !== '').map(v => v.slice(2, -1))
 
-  return styles
+  stylesCache = styles
+
+  return stylesCache
 }
 
 module.exports = {
