4d7c1ef09e954b6b27462eb0b14b0b263c09f79d
Dávid Szabó <david.szabo97@gmail.com>
2017-05-21 21:14:48 +0200

    Remove default from database

--- src/db/sqlite/init.js
+++ src/db/sqlite/init.js
@@ -2,7 +2,7 @@ module.exports = async function init(db) {
   return Promise.all([
     db.runAsync('CREATE TABLE IF NOT EXISTS `paste`' +
       '(`id` INTEGER PRIMARY KEY AUTOINCREMENT,' +
-      '`title` TEXT NOT NULL DEFAULT \'cp\',' +
+      '`title` TEXT NOT NULL,' +
       '`visibility` INTEGER NOT NULL DEFAULT 0,' +
       '`syntax_id` INTEGER NOT NULL,' +
       '`size` INTEGER NOT NULL,' +
@@ -10,7 +10,7 @@ module.exports = async function init(db) {
 
     db.runAsync('CREATE TABLE IF NOT EXISTS `syntax`' +
       '(`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,' +
-      '`name` TEXT UNIQUE NOT NULL DEFAULT \'Just a cp\',' +
+      '`name` TEXT UNIQUE NOT NULL,' +
       '`extension` TEXT NOT NULL,' +
       '`lexer` TEXT NOT NULL )'),
   ])
