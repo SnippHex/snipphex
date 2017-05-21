c2592df173506c73fd3cc5cf14cedd1a9e130883
Dávid Szabó <david.szabo97@gmail.com>
2017-05-21 08:15:57 +0200

    Add error handling

--- src/syntax/router.js
+++ src/syntax/router.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const logger = require('src/logger')
 const syntaxService = require('src/syntax/service')
+const errors = require('restify-errors')
 
 const router = express.Router()
 
 router.get('/syntax', async (req, res) => {
-  const syntaxes = await syntaxService.all()
-  res.json({ data: syntaxes })
+  try {
+    const syntaxes = await syntaxService.all()
+    res.json({ data: syntaxes })
+  } catch (err) {
+    logger.error('Route failed /syntax', { err })
+    res.sendError(new errors.InternalServerError('Failed to fetch syntaxes'))
+  }
 })
 
 module.exports = router
