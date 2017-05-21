6829743ba40830e0adea9eb5dbe95e03dc3f13d0
Dávid Szabó <david.szabo97@gmail.com>
2017-05-21 08:24:20 +0200

    Add error handling

--- src/theme/router.js
+++ src/theme/router.js
@@ -1,14 +1,32 @@
 const express = require('express')
 const themeService = require('./service')
+const logger = require('src/logger')
+const errors = require('restify-errors')
 
 const router = express.Router()
 
 router.get('/theme', async (req, res) => {
-  res.json({ data: await themeService.all() })
+  try {
+    res.json({ data: await themeService.all() })
+  } catch (err) {
+    logger.error('Route failed /theme', { err, name: req.params.name })
+    res.sendError(new errors.InternalServerError('Failed to fetch themes'))
+  }
 })
 
 router.get('/theme/:name/css', async (req, res) => {
-  res.send(await themeService.getCss(req.params.name))
+  try {
+    const css = await themeService.getCss(req.params.name)
+
+    if (!css) {
+      return res.sendError(new errors.NotFoundError('Theme not found'))
+    }
+
+    res.send(css)
+  } catch (err) {
+    logger.error('Route failed /theme/:name/css', { err, name: req.params.name })
+    res.sendError(new errors.InternalServerError('Failed to fetch theme CSS'))
+  }
 })
 
 module.exports = router
