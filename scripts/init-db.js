5b4ebad5e44bf42ce9bc9642944d0c086e499fb7
Dávid Szabó <david.szabo97@gmail.com>
2017-05-21 17:25:16 +0200

    Fix init-db script

--- scripts/init-db.js
+++ scripts/init-db.js
@@ -4,8 +4,15 @@ const initPromise = require('src/db/init')
 const syntaxService = require('src/syntax/service')
 const syntaxes = require('./data/syntaxes')
 
-if (initPromise) {
-  initPromise.then(() => {
-    syntaxes.forEach(v => syntaxService.create(v))
-  })
+async function initDb() {
+  if (initPromise) {
+    return initPromise.then(async () => {
+      for (let i = 0; i < syntaxes.length; i++) {
+        // eslint-disable-next-line no-await-in-loop
+        await syntaxService.create(syntaxes[i])
+      }
+    })
+  }
 }
+
+initDb()
