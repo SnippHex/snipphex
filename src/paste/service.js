5e1986c8e16e5c41ecb82ee35510d297e97541ee
Dávid Szabó <david.szabo97@gmail.com>
2017-05-21 07:27:04 +0200

    Add constants

--- src/paste/service.js
+++ src/paste/service.js
@@ -4,6 +4,12 @@ const syntaxService = require('src/syntax/service')
 const keyCoder = require('./key-coder')
 const pygmentsGenerateHtml = require('src/pygments').generateHtml
 
+const VISIBILITY_PUBLIC = 0
+
+const DEFAULT_TITLE = 'cp'
+const DEFAULT_VISIBILITY = VISIBILITY_PUBLIC
+const DEFAULT_SYNTAX_ID = 1
+
 async function generateHtml(paste) {
   return pygmentsGenerateHtml(content.generateFullPath(paste.id), 'monokai', paste.syntax.lexer)
 }
@@ -36,4 +42,10 @@ module.exports = {
   encodeId: keyCoder.encode,
   decodeKey: keyCoder.decode,
   generateHtml,
+
+  VISIBILITY_PUBLIC,
+
+  DEFAULT_TITLE,
+  DEFAULT_VISIBILITY,
+  DEFAULT_SYNTAX_ID,
 }
