2f390c96ab9045769ea13024d62bd4bc752ef99a
Dávid Szabó <david.szabo97@gmail.com>
2017-08-05 23:28:22 +0200

    Add rate limiting

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ const express = require('express')
 const logger = require('src/logger')
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
+const errors = require('restify-errors')
+const RateLimiter = require('limiter').RateLimiter
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -23,6 +25,18 @@ app.use((req, res, next) => {
 })
 
 app.use(bodyParser.json({ limit: process.env.JSON_REQUEST_SIZE_LIMIT || '1mb' }))
+
+const limiter = new RateLimiter(process.env.LIMITER_COUNT || 5, process.env.LIMITER_INTERVAL || 'second', true)
+app.use((req, res, next) => {
+  limiter.removeTokens(1, (err, remainingTokens) => {
+    if (remainingTokens > 0) {
+      next()
+    } else {
+      return res.sendError(new errors.TooManyRequestsError('Too many requests'))
+    }
+  })
+})
+
 app.use(require('src/paste/router'))
 app.use(require('src/syntax/router'))
 app.use(require('src/theme/router'))
